2fafca6db236059c3596339ba5548bbce5771ad7
A.V.Vignesh Chowdary <74393849+vicky2005-21@users.noreply.github.com>
2024-05-26 21:43:42 +0530

    Update settings.py

--- wallet/settings.py
+++ wallet/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
