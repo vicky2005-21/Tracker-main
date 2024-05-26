d7e5fcb311b86d631d143a67e7d6d781fe07e7e7
A.V.Vignesh Chowdary <74393849+vicky2005-21@users.noreply.github.com>
2024-05-26 22:39:42 +0530

    Update settings.py

--- wallet/settings.py
+++ wallet/settings.py
@@ -14,9 +14,12 @@ import os
 from pathlib import Path
 from .config import *
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
